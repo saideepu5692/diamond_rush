f0cda97cfedfa6a72911e2404bf7ecdcd6517244
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-04-24 03:00:08 +0530

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -21,7 +21,6 @@ if not firebase_admin._apps:
         cred = credentials.Certificate(json_content)
         firebase_admin.initialize_app(cred)
 # Create the input box for the user to enter their answer
-answer2 = st.text_input("Enter your answer here",key="abc")
 # Add the text to the webpage
 text = "While going forward, Bhargav sees an oldman walking with a BLUE STICK and asks if he knew anything about a diamond treasure in this town. The oldman and Bhargav are facing opposite to each other. Seeing the sunset directly, the old man starts remembering something and speaks back. The old man's reply is in the form of audio file below. Listen it carefully and answer the question."
 question="In what direction should Bhargav go now? Please answer in terms on option number only(1 or 2 or 3 or 4)"
@@ -40,6 +39,7 @@ st.write(option1)
 st.write(option2)
 st.write(option3)
 st.write(option4)
+answer2 = st.text_input("Enter your answer here",key="abc")
 st.write("Clue:")
 response = requests.get("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/audio_file.mp3")
 st.audio(response.content, format='audio/mp3')
@@ -48,5 +48,7 @@ st.audio(response.content, format='audio/mp3')
 db = firestore.client()
 db.collection('my_collection').document(st.session_state.get('uid')).update({"answer2":answer2})
 
-if(answer2):
-    st.write("You have entered your input. please return to main page")
+if(answer2=='1'):
+    st.write("Correct! Go along the road to find new hint")
+else:
+    st.write("Wrong!Go to map page and try again")
