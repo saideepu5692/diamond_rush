35d91891e26633cfd422ba261515ba97594b1977
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-04-24 03:17:16 +0530

    Update page4.py

--- pages/page4.py
+++ pages/page4.py
@@ -38,8 +38,8 @@ answer4 = st.text_input("Enter your answer here", key="hint4")
 db = firestore.client()
 db.collection('my_collection').document(st.session_state.get('uid')).update({"answer4":answer4})
 
-
-if(answer4=='MLR'):
-    st.write("Correct! Go along the road to find new hint")
-else:
-    st.write("Wrong!Go to map page and try again")
+if(answer4):
+    if(answer4=='MLR'):
+        st.write("Correct! Go along the road to find new hint")
+    else:
+        st.write("Wrong!Go to map page and try again")
