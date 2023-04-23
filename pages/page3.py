dbc2ac59a6028c95934cd73b3ceca4e5f935d413
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-04-24 00:34:41 +0530

    Add files via upload

---
+++ pages/page3.py
@@ -0,0 +1,50 @@
+import streamlit as st
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+import webbrowser
+from PIL import Image
+import requests
+import json
+# Get the query parameters from the current URL
+params = st.experimental_get_query_params()
+
+# Access the parameter values by key
+st.session_state['uid'] = params.get('uid')[0]
+
+# Define the text to display
+text = "Suddenly, the sky was getting dark. A black cloud appeared in sky. A voice whispered from the sky. You have chosen the wrong route you weakling! Many have walked this way and all of them have perished! Let's test whether you are worthy to findout what is on the other side of your adventure. I will let you go if you answer my question correctly. If not, I shall take your life!"
+question=" Tell me whether the girl died by Suicide or Murder in this photo."
+question2="Help Bhargav investigate the photo and answer the correct option number in answer box.(Answer should be 1 or 2)"
+option1="1.Suicide"
+option2="2.Murder"
+
+# Define the image file to play
+st.header("The Demon")
+st.write(text)
+st.write(question)
+st.write(question2)
+st.write(option1)
+st.write(option2)
+
+# Create the input box for the user to enter their answer
+answer3 = st.text_input("Enter your answer here", key="hint3")
+# Add the text to the webpage
+
+st.write("Clue:")
+#response = requests.get("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/image_hint3.jpg")
+#st.write(response.text)
+#image=Image.open(r'{response.content}')
+st.image("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/image_hint3.jpg")
+#st.image("support/image_hint3.jpg")
+# Get a Firestore client
+db = firestore.client()
+db.collection('my_collection').document(st.session_state.get('uid')).update({"answer3":answer3})
+
+if(answer3):
+    st.write("You have entered your input. please return to main page by clicking below button")
+    if st.button("Go to map"):
+        webbrowser.open("https://www.amazon.in")
+        
+
+
---
+++ pages/page4.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+import webbrowser
+from PIL import Image
+import requests
+
+
+# Define the text to display
+text="The Demon said ,'Very well! You solved the puzzle correctly. Go forward with your adventure young man!' Bhargav continued his journey He came across a young man, telling he knew where the treasure was! When Bhargav asked him about the details, he said,'My friend! I will give you the right direction you need to go to find that treasure. But, I want to test your attention to detail. Let's see what you can do.'"
+question=" Help Bhargav win this puzzle so that he can finally know the location of treasure."
+question2="Watch the video and aswer the questions in form of L,R,M. Here, L stands for 'Left Person', R stands for 'Right Person' and M stands for 'Middle Person'. There will be 3 questions in video. After watching it, Type your answer in answerbox.(Final answer in answer box should be like, LRM or RRM etc.)"
+# Define the image file to play
+st.header("The Stranger")
+st.write(text)
+st.write(question)
+st.write(question2)
+
+# Create the input box for the user to enter their answer
+# Add the text to the webpage
+
+st.write("Clue:")
+#video_file = open(r'C:\Users\saide\OneDrive\Desktop\vs\video_hint4.mp4', 'rb')
+#video_bytes = video_file.read()
+response = requests.get("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/video_hint4.mp4")
+st.video(response.content)
+answer4 = st.text_input("Enter your answer here", key="hint4")
+
+# Get a Firestore client
+db = firestore.client()
+db.collection('my_collection').document(st.session_state.get('uid')).update({"answer4":answer4})
+
+
+if(answer4):
+    st.write("You have entered your input. please return to main page by clicking below button")
+    if st.button("Go to map"):
+        webbrowser.open("https://www.amazon.in")
+        
+
+
