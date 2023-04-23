8e1356265c04de1d7bd91320198a147b24bb5cd5
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-04-23 21:32:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,17 @@ import streamlit as st
 import pyrebase
 import json
 from PIL import Image
-import base64
+import firebase_admin
+from firebase_admin import credentials, firestore
+import requests
+
+# Define a function to check if the login is valid
+def authenticate(username, password):
+    # Replace this with your authentication logic
+    if username == "user" and password == "password":
+        return True
+    else:
+        return False
 
 # Define the login page
 def login():
@@ -20,6 +30,7 @@ def login():
     firebase = pyrebase.initialize_app(firebaseConfig)
     auth = firebase.auth()
     db = firebase.database()
+    st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
     def login_page():
         email = st.text_input("Email", key="login-email")
         password = st.text_input("Password", type="password", key="login-password")
@@ -27,6 +38,7 @@ def login():
             try:
                 # Authenticate the user's credentials
                 user = auth.sign_in_with_email_and_password(email, password)
+                st.session_state["uid"]=user["localId"]
                 st.session_state["authenticated"] = True
                 #s.experimental_rerun()
                 # Redirect to the main page
@@ -83,7 +95,61 @@ def login():
         forgot_password_page()
     else:
         login_page()
-
+def Home():
+    data={}
+    # Initialize a Firebase app
+    if not firebase_admin._apps:
+        # Replace GITHUB_RAW_URL with the raw URL of your .json file in your GitHub repository
+        response = requests.get("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/diamond-rush-0808-firebase-adminsdk-fm0jo-2d5090e23a.json")
+        json_content = response.json()
+        cred = credentials.Certificate(json_content)
+        firebase_admin.initialize_app(cred)
+    db = firestore.client()
+    # Store a variable in Firebase
+    db.collection('my_collection').document(st.session_state.get('uid')).set({})
+    st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+    # Define sidebar contents
+    with st.sidebar:
+        # Add profile icon1fNx44o_jGdQ_WALqYjo_nylaH02yrPx8
+        st.markdown("<div style='text-align: center'><img src='https://drive.google.com/uc?export=view&id=1fNx44o_jGdQ_WALqYjo_nylaH02yrPx8' width='100'></div>", unsafe_allow_html=True)
+        # Add text
+        st.markdown("## Hello!, Welcome to Diamond Rush")
+        st.markdown("Here you can solve the exciting treasure hunt")
+    # Define the CSS style
+    style = """
+    <style>
+    .columnImage {
+        box-sizing: border-box;
+        float: left;
+        width: 107%;
+    }
+    </style>
+    """
+    # Add the CSS style to the page
+    st.markdown(style, unsafe_allow_html=True)
+    # Define the columns
+    col1, col2, col3 = st.columns(3)
+    col4, col5, col6 = st.columns(3)
+    col7, col8, col9 = st.columns(3)
+    # Add content to each column
+    with col1:
+        st.markdown(f"""<a href="#"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1z59GvMgWiFOSJGRF9wP_qL9urclIuv9o"></a>""",unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1iDPB9zJWc_jFErSQV6eFXp4JRU47t1vu"></a>""",unsafe_allow_html=True)
+    with col3:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=13JqXqmJGadtkASS8oKkRpncxyCXnl9XU"></a>""",unsafe_allow_html=True)
+    with col4:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1HmIbxSlXSCit8qJ9VGvLpmbKgutqmZ8F"></a>""",unsafe_allow_html=True)
+    with col5:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1gdakk6gwfVa-0Qw_f-Sitp4zLB8BoiiX"></a>""",unsafe_allow_html=True)
+    with col6:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1Z_bX1lyfaB4JraiUQ9vX8lh4-ao8_Qki"></a>""",unsafe_allow_html=True)
+    with col7:
+        st.markdown(f"""<a href="page1"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=11bUMmqlRJRDsWbfjkwqaD3--jv-DwkKo"></a>""",unsafe_allow_html=True)
+    with col8:
+        st.markdown(f"""<a href="page2"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=12dSWYJO9didCNBMOAoTZkCUlHNFqffB3"></a>""",unsafe_allow_html=True)
+    with col9:
+        st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=13UvmRI3tpKTF0B8KKvYJE7nLmGwSYbn2"></a>""",unsafe_allow_html=True)
 # Define the main page
 def main():
     # Check if the user is authenticated
@@ -93,53 +159,9 @@ def main():
         # Display the main page
         # Set common background image
         # Load image and convert to base64 string
-        st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
-        # Define sidebar contents
-        with st.sidebar:
-            # Add profile icon1fNx44o_jGdQ_WALqYjo_nylaH02yrPx8
-            st.markdown("<div style='text-align: center'><img src='https://drive.google.com/uc?export=view&id=1fNx44o_jGdQ_WALqYjo_nylaH02yrPx8' width='100'></div>", unsafe_allow_html=True)
-            # Add text
-            st.markdown("## Hello!, Welcome to Diamond Rush")
-            st.markdown("Here you can solve the exciting treasure hunt")
+        Home()
 
-        # Define the CSS style
-        style = """
-        <style>
-            .columnImage {
-                box-sizing: border-box;
-                float: left;
-                width: 107%;
-            }
-        </style>
-        """
 
-        # Add the CSS style to the page
-        st.markdown(style, unsafe_allow_html=True)
-
-        # Define the columns
-        col1, col2, col3 = st.columns(3)
-        col4, col5, col6 = st.columns(3)
-        col7, col8, col9 = st.columns(3)
-
-        # Add content to each column
-        with col1:
-            st.markdown(f"""<a href="page1"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1z59GvMgWiFOSJGRF9wP_qL9urclIuv9o"></a>""",unsafe_allow_html=True)
-        with col2:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1iDPB9zJWc_jFErSQV6eFXp4JRU47t1vu"></a>""",unsafe_allow_html=True)
-        with col3:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=13JqXqmJGadtkASS8oKkRpncxyCXnl9XU"></a>""",unsafe_allow_html=True)
-        with col4:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1HmIbxSlXSCit8qJ9VGvLpmbKgutqmZ8F"></a>""",unsafe_allow_html=True)
-        with col5:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1gdakk6gwfVa-0Qw_f-Sitp4zLB8BoiiX"></a>""",unsafe_allow_html=True)
-        with col6:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=1Z_bX1lyfaB4JraiUQ9vX8lh4-ao8_Qki"></a>""",unsafe_allow_html=True)
-        with col7:
-            st.markdown(f"""<a href="page1"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=11bUMmqlRJRDsWbfjkwqaD3--jv-DwkKo"></a>""",unsafe_allow_html=True)
-        with col8:
-            st.markdown(f"""<a href="page2"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=12dSWYJO9didCNBMOAoTZkCUlHNFqffB3"></a>""",unsafe_allow_html=True)
-        with col9:
-            st.markdown(f"""<a href="www.google.com"><img class="columnImage" src="https://drive.google.com/uc?export=view&id=13UvmRI3tpKTF0B8KKvYJE7nLmGwSYbn2"></a>""",unsafe_allow_html=True)
 # Run the app
 if __name__ == "__main__":
     main()
