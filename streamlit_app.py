1e8418814c677ed5159387b0fe5efa108d4e2bd4
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-08-21 22:33:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,120 +2,69 @@ import streamlit as st
 import json
 from PIL import Image
 import firebase_admin
-from firebase_admin import credentials, auth
+from firebase_admin import credentials, firestore, auth
 import requests
-import tempfile
 import pandas as pd
+import datetime
+
+# Set Streamlit page config
 st.set_page_config(
     page_title="My Streamlit App",
     page_icon=":guardsman:",
     layout="wide",
     initial_sidebar_state="collapsed",
 )
-st.header("Welcome to Diamond Rush!")
-# Fetch Firebase credentials JSON from GitHub
-credentials_url = "https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/diamond-rush-0808-firebase-adminsdk-fm0jo-2d5090e23a.json"
-response = requests.get(credentials_url)
+
+# Initialize Firebase app
+response = requests.get("https://raw.githubusercontent.com/saideepu5692/diamond_rush/main/support/diamond-rush-0808-firebase-adminsdk-fm0jo-2d5090e23a.json")
 json_content = response.json()
+cred = credentials.Certificate(json_content)
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-# Check if the Firebase app is already initialized
-if not firebase_admin._apps:
-    # Create a temporary file to write the JSON content
-    temp_json_file = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
-    temp_json_file.write(response.content)
-    temp_json_file.close()
+# Define the login page
+def login():
+    st.markdown("<style> ul {display: none;} </style>", unsafe_allow_html=True)
+    st.header("Welcome to Diamond Rush!")
 
-    # Initialize Firebase SDK
-    cred = credentials.Certificate(temp_json_file.name)
-    firebase_admin.initialize_app(cred)
+    email = st.text_input("Email", key="login-email")
+    password = st.text_input("Password", type="password", key="login-password")
+    if st.button("Login"):
+        try:
+            user = auth.sign_in_with_email_and_password(email, password)
+            expiration_time = datetime.datetime.now() + datetime.timedelta(hours=1)
+            st.experimental_set_query_params(authenticated=True, uid=user["localId"], expires=expiration_time)
+        except:
+            st.error("Authentication failed. Please check your credentials and try again.")
+    if st.button("Sign Up"):
+        st.session_state['signup'] = True
+    if st.button("Forgot Password?"):
+        st.session_state['forgot_password'] = True
 
-    st.write("Firebase SDK initialized successfully!")
+# Define the Home page
+def Home():
+    st.header("Welcome to Diamond Rush - Home")
+    # Your Home page content goes here
 
-    # Clean up temporary file
-    temp_json_file.unlink()
-else:
-    st.write("Firebase SDK is already initialized!")
+# Define the Admin page
+def admin():
+    st.header("Admin Page")
+    # Your admin page content goes here
 
-# Rest of your Streamlit app code
+# Define the main page
+def main():
+    params = st.experimental_get_query_params()
+    if "expires" in params and datetime.datetime.now() < datetime.datetime.fromisoformat(params["expires"][0]):
+        st.session_state.authenticated = True
+        st.session_state.uid = params["uid"][0]
 
-# Streamlit UI for login page
-st.title("Login Page")
-
-# User input fields
-username = st.text_input("Enter Username")
-password = st.text_input("Enter Password", type="password")
-
-# Sign-in button
-login_button = st.button("Sign In")
-
-# Create user button
-create_user_button = st.button("Create User")
-
-# Forgot password button
-forgot_password_button = st.button("Forgot Password")
-
-# Create a session state dictionary to store session-related information
-if "session_state" not in st.session_state:
-    st.session_state.session_duration = 3600  # 1 hour in seconds
-    st.session_state.logged_in = False
-    st.session_state.is_admin = False
-
-# Login function
-def login(email, password):
-    try:
-        user = firebase_admin.auth.get_user_by_email(email)
-        auth_user = firebase_admin.auth.verify_password_reset_code(password)
-        if auth_user.uid == user.uid:
-            st.session_state.logged_in = True
-            st.session_state.user_email = email
-            st.success("Login successful!")
-            # Redirect to the home page with images
-        else:
-            st.error("Invalid credentials.")
-    except firebase_admin.auth.AuthError as e:
-        if "USER_NOT_FOUND" in str(e):
-            st.error("User does not exist.")
-        else:
-            st.error("Invalid credentials.")
-
-# Create user function
-def create_user(email, password):
-    try:
-        user = auth.create_user(email=email, password=password)
-        st.success("User created successfully!")
-    except Exception as e:
-        if "EMAIL_EXISTS" in str(e):
-            st.error("Email already exists.")
-        else:
-            st.error("Failed to create user.")
-
-# Forgot password function
-def forgot_password(email):
-    try:
-        auth.generate_password_reset_link(email)
-        st.success("Password reset link sent to your email.")
-    except Exception as e:
-        if "USER_NOT_FOUND" in str(e):
-            st.error("User does not exist.")
-        else:
-            st.error("Failed to send reset link.")
-
-# Handle button clicks
-if create_user_button:
-    create_user(username, password)
-
-if forgot_password_button:
-    forgot_password(username)
-
-if login_button:
-    login(username, password)
-
-# If user is logged in, display home page
-if st.session_state.logged_in:
-    # If user is admin, display admin page
-    if st.session_state.is_admin:
-        st.success("Welcome, Admin!")
-        # Add logic for displaying admin-specific functionality
+    if "admin" in st.session_state:
+        admin()
+    elif "authenticated" in st.session_state and st.session_state.authenticated:
+        Home()
     else:
-        st.success("Welcome")
-        # Add logic for displaying home page with images
+        login()
+
+# Run the app
+if __name__ == "__main__":
+    main()
