2306af93b3c993c51c2fcfc5d5a24bca89e3d5ae
saideepu5692 <101350382+saideepu5692@users.noreply.github.com>
2023-08-21 22:39:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import json
 import firebase_admin
-from firebase_admin import credentials, firestore, auth
+from firebase_admin import credentials,auth
 from PIL import Image
 import requests
 import pandas as pd
 import datetime
+from google.cloud import firestore
 
 # Set Streamlit page config
 st.set_page_config(
